ae63b90e8356c2b6e69860d1faae32089ded6852
chris30269 <cernst3@gatech.edu>
2017-01-31 16:11:42 -0500

    details are important

--- farts.js
+++ farts.js
@@ -174,7 +174,7 @@ $(function(){
 	//don't put my email on blast
 	var poop = 12/4;
 	var boogers = 'chris'+(poop-3);
-	$("#lookAtMe").attr("href", "mailto:"+boogers.substr(0,5)+"@chrisernst.me?subject=I%20want%20to%20give%20you%20money").html(boogers+"@chrisernst.me");
+	$("#lookAtMe").attr("href", "mailto:"+boogers.substr(0,5)+"@chrisernst.me?subject=I%20want%20to%20give%20you%20money").html(boogers.substr(0,5)+"@chrisernst.me");
 });
 
 function hover(which){
